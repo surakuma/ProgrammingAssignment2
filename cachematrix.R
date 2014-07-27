6cd48f99ca3d38d39d49d0014b78a40e1cdaa27e
Suraj Kumar <suraj.kumar@inria.fr>
2014-07-27 23:18:54 +0200

    The basic model

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    m<- NULL
+    set<- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get<- function() x
+    setsolution <- function(sol) m <<- sol
+    getsolution <- function() m
+    list(set = set, get=get, setsolution=setsolution, getsolution=getsolution)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m<-x$getsolution()
+    if(!is.null(m))
+    {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m<- solve(x)
+    x$setsolution(m)
+    m
 }
